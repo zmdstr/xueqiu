ab5120fcf71ae663f43cba722c3b8282189c56c8
zmdstr <zhenyang0510@163.com>
2018-04-02 22:12:50 +0800

    优化，增加循环判断、安全退出、类型转换、输出格式化等

--- xueqiu.py
+++ xueqiu.py
@@ -1,13 +1,15 @@
 from salary_increase import count_money
-print('''定投十年翻十倍！
-开始定投吧！
-请选择你的定投计划：
-0.个人加薪计划;
-1.待创建;''')
-option = int(input())
-if option == 0:
-    pe = int(input('请输入 PE '))
-    result = count_money(pe)
-    print('''经计算，本期应投入金额为''' + str(result))
-else:
-    print('待开发！')
+
+while True:
+    print('''
+    定投十年翻十倍!
+    开始定投吧!
+    0.个人加薪计划,
+    1.退出;''')
+    option = int(input('请选择你的定投计划:\n'))
+    if option == 0:
+        pe = float(input('请输入市盈率(PE):\n'))
+        result = count_money(pe)
+        print('''经计算，本期应投入金额为: %d 元！''' % result)
+    else:
+        break
